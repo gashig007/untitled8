673794690899ebf8e18fb3398461fa841b393546
gashig007 <rustamgashig@gmail.com>
2021-12-07 16:46:37 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 34, -7, -13, 3, 0};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
